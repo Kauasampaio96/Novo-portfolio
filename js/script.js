badf8a882233b628b2c63bcd1c8d3247956e9e3b
Kauasampaio96 <66958992+Kauasampaio96@users.noreply.github.com>
2020-11-07 12:59:07 -0300

    Add files via upload
    Colocando arquivos no repositório

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let astro = document.querySelector('#img-astro')
+
+function clicar(){
+    astro.style.transform = 'scale(3.15,3.15)'
+    astro.style.left = '500px'
+}
+
+function diminuir(){
+    astro.style.transform = 'scale(1,1)'
+    astro.style.left = '0px'
+}
